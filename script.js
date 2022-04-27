f2d5f5efde76b027bbfc8f8aa9ebc7228f5a258f
megz15 <meghraj.g16@gmail.com>
2022-04-27 15:08:56 +0530

    Added loop

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 dict = {
     'rock': 0,
     'paper': 1,
-    'scissors': 2,
+    'scissor': 2,
 }
 
 function computerPlay() {
@@ -13,28 +13,48 @@ const rl = require('readline').createInterface({
     output: process.stdout
 });
 
-let player_choice;
+function game() {
+    let player_choice;
 
-rl.question("Your choice: ", function(choice) {
-    player_choice = choice;
-    rl.close();
-});
+    rl.question("\nYour choice: ", function (choice) {
+        player_choice = choice.toLowerCase();
 
-rl.on("close", function() {
-    computer_choice = computerPlay()
-    console.log("\nYou chose " + player_choice);
-    console.log("Computer chose", computer_choice);
-
-    player_choice = dict[player_choice]
-    computer_choice = dict[computer_choice]
-
-    if (player_choice === computer_choice) {
-        console.log("It's a tie!");
-    } else {
-        if ((player_choice+1) % 3 === computer_choice) {
-            console.log("Computer wins!");
+        if (!(player_choice in dict)) {
+            console.log("\nInvalid choice");
+            game();
         } else {
-            console.log("You win!");
+            computer_choice = computerPlay()
+            console.log("\nYou chose " + player_choice);
+            console.log("Computer chose", computer_choice);
+
+            player_choice = dict[player_choice]
+            computer_choice = dict[computer_choice]
+
+            if (player_choice === computer_choice) {
+                console.log("It's a tie!");
+            } else {
+                if ((player_choice + 1) % 3 === computer_choice) {
+                    console.log("Computer wins!");
+                } else {
+                    console.log("You win!");
+                }
+            }
         }
-    }
-});
+
+        rl.question("\nPlay again? (y/n): ", function (answer) {
+            if (answer.toLowerCase() === 'y') {
+                game();
+            } else {
+                rl.close();
+            }
+        });
+
+        // rl.close();
+    });
+
+    rl.on("close", function () {
+        console.log("\nThanks for playing!");
+    });
+}
+
+game();
